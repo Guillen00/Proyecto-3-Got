e7bb2c8225f9dd23338d2ca1fd341ec6c08d284d
Armando <armandofg3937@gmail.com>
2020-07-29 19:28:27 -0600

    /commit

--- api/src/routes/links.js
+++ api/src/routes/links.js
@@ -1,17 +1,15 @@
 const express = require('express');
-
 const router = express.Router();
-
-var jsdiff = require('diff');
+var  jsdiff = require('diff');
 var md5 = require('md5');
 let lastcommitid=0;
 
 const pool = require('../database');
 
 router.get('/get', (req, res) => {
-    const { repositorio } = req.body.name;
-    const { file } = req.body.file;
-     pool.query('SELECT * FROM Repositorio WHERE ID = ? ', [repositorio], (err, rows) => {
+    const { repositorio } = req.body;
+    const { file } = req.body;
+     pool.query("SELECT * FROM "+[file]+"_"+[repositorio] , (err, rows) => {
         if (!err) {
           res.json(rows[0]);
         } else {
@@ -21,61 +19,66 @@ router.get('/get', (req, res) => {
 });
 
 router.post('/init', async (req, res) => {
-    const {id} = req.body.name;
-    var tablaRepositorio = "CREATE TABLE IF NOT EXISTS Repo_"+ [id] + "( lastcommit VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , nameFile VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , PRIMARY KEY (nameFile))";
-    pool.query(tablaRepositorio);
+    const {name} = req.body;
+    var tablaRepositorio = "CREATE TABLE IF NOT EXISTS Repo_"+ [name] + "( lastcommit VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , nameFile VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , PRIMARY KEY (nameFile))";
+    await pool.query(tablaRepositorio);
     await pool.query('INSERT INTO Repositorio set ? ', req.body).then(res.send({status: "success"})).catch(error => res.send("Error"));
 });
 
 router.post('/commit',  (req, res) => {
-    const {repositorio} = req.body.name;
-    const {mensaje} = req.body.message;
-    const {fileName} = req.body.files; //Es un array
-    const {fileContent} = req.body.fileContent;//Es un array
-    const {commitID} = req.body.commitID;
+    const {name} = req.body;
+    const {message} = req.body;
+    const {files} = req.body; //Es un array
+    const {fileContent} = req.body;//Es un array
+    const {commitID} = req.body;
+    var commitClient = [commitID];
+    var commitServer;
 
-    var verificarCommit;
-    var lastcommit = "Ultimo";
+    pool.query('SELECT * FROM Repo_' + [name], (err, rows) => {
+        var {lastcommit} = rows[0];
+        commitServer = [lastcommit]; 
 
-    pool.query('SELECT * FROM Repo_' + [repositorio], (err, rows) => {
-      if (!err) {
-        verificarCommit = rows[0];
-        lastcommit = verificarCommit.commit;
-      } 
-    });
+        if ( commitClient[0] == commitServer[0]){
+          for(var i=0; i<files.length;i++){
+            var sql = "CREATE TABLE IF NOT EXISTS "+ files[i]+ "_"+ [name] + "( files VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , newLinesAt VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , deletedLinesAt VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL, newLines VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,message VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , commitID VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , name VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , PRIMARY KEY (files))";
+            pool.query(sql);
+            
+            var j=0;
+            var newLinesAt=[];
+            var deletedLinesAt=[];
+            var newLines=[];
+            console.log(fileContent[0]);
+            var diff= jsdiff.diffTrimmedLines("hola", fileContent[i]);
+            diff.forEach(function(part){
+                if(part.added){
+                  //Se agrega esta parte
+                  newLinesAt.push(j);
+                  newLines.push(part);
+                }else if(part.deleted){
+                  //se quita esta parte
+                  deletedLinesAt.push(j);
+                }
+                j++;
+                //Como se guardaria esto?
+            });
+           console.log(newLines)
+           console.log(newLinesAt)
+            console.log(deletedLinesAt);
+
+            var sqlCommit = "INSERT INTO Repo_" + [name] + "(lastcommit, nameFile) VALUES ('" + md5(lastcommitid) + "', '"+ files[i] +"')";
+            pool.query(sqlCommit);
+            var data = "INSERT INTO "+ files[i]+ "_"+ [name] +" (files, newLinesAt, deletedLinesAt, newLines, message, commitID, name) VALUES ('"+ files[i] +"','"+newLinesAt+"','"+deletedLinesAt+"','"+newLines+"','"+[message]+"','"+md5(lastcommitid)+"','"+[name]+"')";
+            pool.query(data);  
+          }
+          res.send({status: "success", commiIDs: md5(lastcommitid)});
     
-    if (lastcommit == [commitID]){
-      for(var i=0; i<fileName.length;i++){
-        /* 
-        var j=0;
-        var newLinesAt=[];
-        var deletedLinesAt=[];
-        var newLines[];
-        var diff= JsDiff.diffTrimmedLines(oldCommit, newCommit)
-        diff.forEach(function(part){
-            if(part.added){
-              //Se agrega esta parte
-              newLinesAt.push(j);
-              newLines.push(part);
-            }else if(part.deleted){
-              //se quita esta parte
-              deletedLinesAt.push(j);
-            }
-            j++;
-            //Como se guardaria esto?
-        });
-        */
-        var sqlCommit = "INSERT INTO Repo_" + [repositorio] + "(commit, nameFile) VALUES (" + [md5(lastcommitid)] + ", "+ [fileName[i]] +")";
-        var sql = "CREATE TABLE IF NOT EXISTS "+ [fileName[i]]+ "_"+ [repositorio] + "( nombre VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , datos VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , commit VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , idcommit VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , repositorio VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL , PRIMARY KEY (nombre))";
-        pool.query(sql);
-        pool.query(sqlCommit);
-        pool.query('INSERT INTO '+ [fileName[i]]+ "_"+ [repositorio] +' set ? ', fileContent[i]);  
-      }
-      res.send({status: "success", commiIDs: md5(lastcommitid)});
-    }else{
-      res.send({status:"rejected", reason : "Different Commit Version on Server" });
-    }   
-    lastcommitid++;
+        }else{
+          res.send({status:"rejected", reason : "Different Commit Version on Server" });
+        }   
+        lastcommitid++;
+    });
+     
+    
 });
 
 router.get('/status', (req, res) => {
@@ -110,7 +113,7 @@ router.get('/status', (req, res) => {
   }
 })
 
-app.put('/rollback', function (req, res) {
+router.put('/rollback', function (req, res) {
   const{repositorio} = req.body.name;
   const{fileName} = req.body.file;
   const{commitID} = req.body.commitID; //Commit al cual volver
